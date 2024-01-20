8cc0a1a1f2cb4a87b72c9b5b46c50517dd0582ae
brdevs-tm <bright19.off@gmail.com>
2024-01-20 23:37:45 +0500

    new commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 const passwordInput = document.querySelector(".password-row input");
 const showIcon = document.querySelector(".show-icon");
 
-showIcon.addEventListener("click", () => {
-  if (passwordInput.type === "password") {
-    showIcon.src = "../assets/eye.svg";
-    passwordInput.type = "text";
+const loginBtn = document.querySelector(".login-btn");
+
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".exit");
+
+const successModal = document.querySelector(".succesfull");
+
+let regex =
+  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{}|;:'",.<>?/]).{8,}$/;
+
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (regex.test(passwordInput.value)) {
+    successModal.style = "opacity: 1; z-index: 9999";
+    window.location.href = "https://severyanochka-new.netlify.app/";
   } else {
-    showIcon.src = "../assets/show-pass.svg";
-    passwordInput.type = "password";
+    modal.style = "top: 10px";
   }
+});
+
+closeModal.addEventListener("click", () => {
+  modal.style = "top: -200px";
 });
